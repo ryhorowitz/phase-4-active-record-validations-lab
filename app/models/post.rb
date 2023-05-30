8e83ffeb6fc5e8a1adf56038f5eb50b66300eeca
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-05-30 17:51:48 -0400

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :title_is_clickbaity
+
+  def title_is_clickbaity
+    clickbait = [/Won't Believe/i, /Secret/i, /Top \d+/i, /Guess/i]
+    if clickbait.none? { |regex| regex.match title }
+      errors.add(:title, "Sorry")
+    end
+    # regex = /(?:(Won't Believe)|(Secret)|(Top \d+)|(Guess))/
+  end
 end
